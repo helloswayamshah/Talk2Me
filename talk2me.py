eed5c53251bfcddf42f2ac4ea3412cbe7e6e7890
Swayam <helloswayamshah@gmail.com>
2023-05-31 18:19:57 -0700

    Initial Commit!

---
+++ talk2me.py
@@ -0,0 +1,43 @@
+import discord
+from discord import app_commands
+import google.generativeai as palm
+import os
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+print(os.environ)
+
+api_key = os.environ["api_key"]
+client_key = os.environ["client_key"]
+
+
+
+palm.configure(api_key=api_key)
+
+@tree.command(name = "hello", description = "My first application Command") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def first_command(interaction):
+    await interaction.response.send_message(f"Hello! {interaction.user.mention}")
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    content = message.content
+    print(content)
+    response = palm.chat(prompt=[content], model = "models/chat-bison-001")
+    print(response)
+    await message.channel.send(response.last[0:2000])
+    response.reply(content)
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print("Ready!")
+
+# Run the bot
+client.run(client_key)
+
