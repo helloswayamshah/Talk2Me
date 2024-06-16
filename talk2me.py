6b57f53df29d02795bc887acca4d2abc53dac408
Swayam <34980258+helloswayamshah@users.noreply.github.com>
2024-06-15 18:52:06 -0700

    Update talk2me.py

--- talk2me.py
+++ talk2me.py
@@ -18,11 +18,11 @@ client_key = os.environ["client_key"]
 
 palm.configure(api_key=api_key)
 
-@client.command(name = "hello", description = "My first application Command") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "hello", description = "My first application Command") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction):
     await interaction.response.send_message(f"Hello! {interaction.user.mention}")
 
-@client.command(name = "test", description = "My second application Command") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "test", description = "My second application Command") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def second_command(interaction):
     await interaction.response.send_message("testing...")
 
