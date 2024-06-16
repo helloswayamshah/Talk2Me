3b29dd8d864d0bb80c1c62ae381cd9d786d9ef11
Swayam <34980258+helloswayamshah@users.noreply.github.com>
2024-06-15 18:48:13 -0700

    fixed command

--- talk2me.py
+++ talk2me.py
@@ -4,7 +4,7 @@ import google.generativeai as palm
 import os
 
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
@@ -22,9 +22,9 @@ palm.configure(api_key=api_key)
 async def first_command(interaction):
     await interaction.response.send_message(f"Hello! {interaction.user.mention}")
 
-@client.command(name = "test")
+@client.command(name = "test", description = "My second application Command") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def second_command(interaction):
-    await interaction.response.send_messahe("testing...")
+    await interaction.response.send_message("testing...")
 
 @client.event
 async def on_message(message):
